219bdd59dd628e67d58ce2b7a9b753d2a71e3d65
Ginaronch <alexandr.yarosh1@gmail.com>
2025-03-29 18:47:59 +0100

    version 1.0

--- 11.2(2).py
+++ 11.2(2).py
@@ -2,7 +2,7 @@ def generate_cube_numbers(end):
     num = 2
     while num ** 3 <= end:
         yield num ** 3
-        num += 1
+        num = num + 1
 from inspect import isgenerator
 gen = generate_cube_numbers(1)
 assert isgenerator(gen) == True
